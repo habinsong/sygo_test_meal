7fdaf8a77c59636d0119b710e2b234fb5d6c43b5
HABIN959 <habin959@gmail.com>
2018-05-14 13:14:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
+	
+	error_reporting(0);                           // error reporting disable
+        header("Content-type: application/json; charset=UTF-8"); 
 
 	$text = $json->result->parameters->text;
 
